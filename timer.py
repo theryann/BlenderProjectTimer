d9fd798943eeed973e1655d88338bc265ea37460
Darian Patzak <my-dev@outlook.de>
2024-10-22 21:44:48 +0200

    initial commit

---
+++ timer.py
@@ -0,0 +1,17 @@
+bl_info = {
+    "name": "Blender Project Timer",
+    "author": "Darian Patzak",
+    "version": (1, 0, 0),
+    "blender": (4, 0, 0),
+    "location": "Info",
+    "description": "Log and display time spend on Blender Projects in this directory",
+    "warning": "",
+    "wiki_url": "",
+    "tracker_url": "",
+    "category": "Interface"
+}
+
+import bpy
+import json
+import time
+
