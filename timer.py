5cf17f9d7d172707e3de5cc305f1c8fdf1292ed6
Darian Patzak <my-dev@outlook.de>
2024-10-23 01:01:32 +0200

    replace elapsed time of current session in log rather than appending multiple records with the same start time

--- timer.py
+++ timer.py
@@ -24,6 +24,8 @@ last_activity_s: int = None         # timestamp of last activity in epoch second
 
 total_time_s: int = None            # total time spend in project since opening file (minus inactivity)
 
+currently_active: bool = None
+
 SAVE_FILE_NAME: str = 'log.json'
 blend_file_name: str = None
 
@@ -31,6 +33,11 @@ def save_working_time_to_json() -> None:
     """ load log file, add new elapsed time and save """
     global start_time_s, last_activity_s
     global blend_file_name, SAVE_FILE_NAME
+    global currently_active
+
+    # abort if currently inactive (else the counter would increase while actually inactive)
+    if not currently_active:
+        return
 
     log_file_path: str = os.join( os.getcwd(), SAVE_FILE_NAME )
     log_file: dict = None
@@ -53,13 +60,31 @@ def save_working_time_to_json() -> None:
     begin_string: str = time.strftime('%FT%H:%M:%S', time.localtime( start_time_s ))
     end_string: str   = time.strftime('%FT%H:%M:%S', time.localtime( start_time_s + elapsed_time_s ))
 
-    # add current session to list of all sessions
-    log_file["all_sessions"].append({
-        "file": blend_file_name,
-        "starttime": begin_string,
-        "endtime": end_string,
-        "minutes_elapsed": elapsed_time_s
-    })
+    # update currents session elapsed time if it's already recorded
+    # "session recorded" means the begin_string
+    curr_session_found = False
+    
+    for i, session in enumerate(log_file["all_sessions"]):
+        if not session.get("file") == blend_file_name:
+            continue
+        if not session.get("starttime") == begin_string:
+            continue
+
+        # begin_string found => so replace time elapsed
+        log_file["all_sessions"][i]["endtime"] = end_string
+        log_file["all_sessions"][i]["minutes_elapsed"] = elapsed_time_s
+
+        curr_session_found = True
+        break
+
+    # add current session to list of all sessions since it's not recorded yet
+    if not curr_session_found:
+        log_file["all_sessions"].append({
+            "file": blend_file_name,
+            "starttime": begin_string,
+            "endtime": end_string,
+            "minutes_elapsed": elapsed_time_s
+        })
 
     # add elapsed time to counter of this blend file
     # and sum all files to new total time
@@ -103,7 +128,7 @@ def ui_draw_elapsed_time(self, context) -> None:
 
 
 def register():
-    global start_time_s, total_time_s, last_activity_s
+    global start_time_s, total_time_s, last_activity_s, currently_active
 
     # bpy.utils.register_class(TIME_OT_reset)
 
@@ -117,6 +142,7 @@ def register():
     start_time_s = int( time.time() )
     total_time_s = 0
     last_activity_s = start_time_s
+    currently_active = True
 
     # track the user input events to track the activity
     bpy.app.handlers.depsgraph_update_post.append( track_activity )
