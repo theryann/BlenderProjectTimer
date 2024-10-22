b8b5e4c9c73045295d0d41d306656199cd9ab37a
Darian Patzak <my-dev@outlook.de>
2024-10-23 01:00:58 +0200

    replace elapsed time of current session in log rather than appending multiple records with the same start time

--- timer.py
+++ timer.py
@@ -19,14 +19,19 @@ import os
 INACTIVE_TIMEOUT: int = 60 * 3      # 3 minutes before counted as inactive
 CHECK_INTERVAL: int = 10            # 10 seconds between checking if still working
 
-start_time_s: int = None            # start time in epoch seconds to begin counting from (resets after each inactivity)
-curr_session_time_s: int = None     # the seconds spend working on the current project
+start_time_s: int = None            # start timestamp in epoch seconds to begin counting from (resets after each inactivity)
+last_activity_s: int = None         # timestamp of last activity in epoch seconds
+
+total_time_s: int = None            # total time spend in project since opening file (minus inactivity)
 
 SAVE_FILE_NAME: str = 'log.json'
 blend_file_name: str = None
 
 def save_working_time_to_json() -> None:
     """ load log file, add new elapsed time and save """
+    global start_time_s, last_activity_s
+    global blend_file_name, SAVE_FILE_NAME
+
     log_file_path: str = os.join( os.getcwd(), SAVE_FILE_NAME )
     log_file: dict = None
 
@@ -44,9 +49,9 @@ def save_working_time_to_json() -> None:
     with open( log_file_path, 'r' ) as fp:
         log_file = json.load( log_file_path )
 
-    elapsed_time_s: int = start_time_s + curr_session_time_s
+    elapsed_time_s: int = last_activity_s - start_time_s
     begin_string: str = time.strftime('%FT%H:%M:%S', time.localtime( start_time_s ))
-    end_string: str   = time.strftime('%FT%H:%M:%S', time.localtime( elapsed_time_s ))
+    end_string: str   = time.strftime('%FT%H:%M:%S', time.localtime( start_time_s + elapsed_time_s ))
 
     # add current session to list of all sessions
     log_file["all_sessions"].append({
@@ -70,7 +75,57 @@ def save_working_time_to_json() -> None:
     with open( log_file_path, 'w' ) as fp:
         json.dump( log_file, fp, indent=4 )
 
+def update_timer() -> int:
+    """
+    gets called every x seconds (CHECK_INTERVAL)
+    and calls for the redrawing of the timer and the saving of the logfile
+    @return: the number of seconds until next call of this function 
+    """
+    global CHECK_INTERVAL, INACTIVE_TIMEOUT
+    global last_activity_s
 
-    
+    # fetch current time as epoch timestamp on seoconds
+    current_time_s: int = int( time.time() )
+
+    # time of inactivity longer than timeout 
+    if last_activity_s + current_time_s > INACTIVE_TIMEOUT:
+        ...
+        
+
+    return CHECK_INTERVAL
 
 
+def ui_draw_elapsed_time(self, context) -> None:
+    """draws the currently elapsed time to the Blender UI"""
+    global total_time_s
+    minutes, seconds = divmod(total_time_s, 60)
+    self.layout.label(text=f"{minutes:2}:{seconds:2}")
+
+
+def register():
+    global start_time_s, total_time_s, last_activity_s
+
+    # bpy.utils.register_class(TIME_OT_reset)
+
+    # add timer to 3D view
+    bpy.types.VIEW3D_HT_header.append( ui_draw_elapsed_time )
+
+    # add blender internal timer to repeatedly call the update functions
+    bpy.app.timers.register( update_timer )
+
+    # initalize time variables
+    start_time_s = int( time.time() )
+    total_time_s = 0
+    last_activity_s = start_time_s
+
+    # track the user input events to track the activity
+    bpy.app.handlers.depsgraph_update_post.append( track_activity )
+
+def unregister():
+    # bpy.utils.unregister_class(TIME_OT_reset)
+    bpy.types.VIEW3D_HT_header.remove( ui_draw_elapsed_time )
+    bpy.app.timers.unregister( update_timer )
+    bpy.app.handlers.depsgraph_update_post.remove( track_activity )
+
+if __name__ == '__main__':
+    register()
